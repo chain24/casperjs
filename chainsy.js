8d9ba336232b4ffce8de7bc6d67dc8e1d47f50f6
chainw <chainsy97@gmail.com>
2018-12-21 10:43:48 +0800

    首次登陆测试

---
+++ chainsy.js
@@ -0,0 +1,18 @@
+var casper = require('casper').create();
+casper.start('https://ksc.dodoedu.com', function() {
+    this.fill('form[class="chiActiveInfo"]', {
+        'userName': 'G421127200705260461',
+        'userPwd': '654321'
+    }, false);
+});
+
+casper.then(function() {
+    this.click('input[class="btn btn-blue btn-block btn-lg js-sub"]');
+
+    this.echo('clicked...');
+});
+
+casper.then(function() {
+    this.echo(this.getTitle());
+});
+casper.run();
