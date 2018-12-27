7475f382238181e9e5cee0f87218dced474109d0
chainw <chainsy97@gmail.com>
2018-12-27 10:02:16 +0800

    测试

--- chainsy.js
+++ chainsy.js
@@ -1,18 +1,35 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'debug',
+    onError: function(self,m){
+        console.log("FATAL:" + m);
+        self.exit();
+    }
+});
+casper = require('casper').create();
 casper.start('https://ksc.dodoedu.com', function() {
     this.fill('form[class="chiActiveInfo"]', {
-        'userName': 'XXXX',
+        'userName': 'L421127200908040119',
         'userPwd': '654321'
     }, false);
 });
 
 casper.then(function() {
     this.click('input[class="btn btn-blue btn-block btn-lg js-sub"]');
-
     this.echo('clicked...');
 });
 
+casper.wait(2000,function(){
+    this.evaluate(function(){
+        document.getElementById("submitBtnID").click();
+    });
+});
+
 casper.then(function() {
-    this.echo(this.getTitle());
+    this.click('input[class="submit"]');
+});
+casper.then(function() {
+    this.click('a[class="btn btn-comment"]');
+    this.echo('start to exam...');
 });
 casper.run();
